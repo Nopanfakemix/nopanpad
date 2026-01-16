df67e627dd8810e6e5e0e7e115c78d3a9719f9b3
Nopanfakemix <nopanfakemix@duck.com>
2026-01-15 22:44:17 -0500

    Update Calc V1.ino
    Changed again! working in Wokwi widda 20x4 LCD for diag, gotta go to sleep and need to save my code nobody is ever gonna read this not even me! If anyone in the future wanted this note to be helpful you can go back in time and fight me about it

--- v1.3/Code/Calc V1.ino
+++ v1.3/Code/Calc V1.ino
@@ -1,32 +1,33 @@
-/*
-this code by itself should run operate as a calculator using (not yet) existing nopanpad hardware.
-no idea how I switch from numpad mode to standalone calculator mode!
-*/
+//shitass!
 
 #include <Keypad.h>
 #include <SPI.h>
+#include <LiquidCrystal.h>
 
 //keypad
-const byte ROWS = 4;
+const byte ROWS = 5;
 const byte COLS = 4;
 
 char numkeys[ROWS][COLS] = {
+{'N', '/', '*', 'D'},
 {'7', '8', '9', '-'},
 {'4', '5', '6', '+'},
-{'1', '2', '3', '*'},
-{'D', '0', '.', '='},
+{'1', '2', '3', 'X'},
+{'0', 'X', '.', '='},
 };
 
 byte colPins[COLS] = {6, 7, 8, 9};
-byte rowPins[ROWS] = {2, 3, 4, 5}; //pin numbers on the SOM I's using
+byte rowPins[ROWS] = {1, 2, 3, 4, 5}; //pin numbers on the SOM I's using
 
 Keypad kpd = Keypad(makeKeymap(numkeys), rowPins, colPins, ROWS, COLS);
+
+
+LiquidCrystal lcd(21,20,19,18,17,16);
 /*
 //how tf do I setup SPI i never done this before
 // 2.9" Display, B/W, 128*296
 //SDA=7, CS=8, RES=9, BUSY=10, D/C=11, SCL=11 <<planned display pins for current E-ink display and PCB design
 //SPI display
-
 */
 
 //dunno the right terms but these get assigned values to make calculations and displays
@@ -38,17 +39,21 @@ float EX;  //swap area
 char key, oper;
 uint8_t Ct=0; //character count
 uint8_t Dt=0; //after decimal count
+bool  calc=false;
 
 String SEX0 = String(""); //string of above expressions 4 print to display
 String SEX1 = String(""); //string expression
 String SEX2 = String(""); //haha funneeeeeeee
+String SEX3 = String("");
 
 
 void setup(){
-  Serial1.begin(115200);
-  Serial1.println("FUCK");
+  Serial1.begin(9600);
+  lcd.begin(20, 4);
+  // you can now interact with the LCD, e.g.:
+  lcd.print("Hello World!");
   delay(1000);
-  Serial1.println("this");
+  lcd.clear();
 }
 
 void loop(){
@@ -60,11 +65,14 @@ void loop(){
 }
 
 void DoMath(){              //okay here we go this is where i try to write code to make
-  if (key=='1'||key=='2'||key=='3'||key=='4'||key=='5'||key=='6'||key=='7'||key=='8'||key=='9'||key=='0'||key=='.'){   //if any number or decimal
+  if (key=='1'||key=='2'||key=='3'||key=='4'||key=='5'||key=='6'||key=='7'||key=='8'||key=='9'||key=='0'||key=='.'){  //if any number or decimal
+    if (calc){SEX0=String("");SEX1=String("");}
     if ((Ct==0)&&(key=='0')&&(Dt==0)){Ct=0;} else {    //if
       if(key=='.'){if(Dt==0){SEX0= SEX0 + String(key);Dt++;}}  //if '.', then if Dt=0, add '.' as string to SEX1
       else{ SEX0= SEX0 + String(key);if (Dt>0){Dt++;} else {Ct++;}}    //this will probably misbehave if you hit 0 multiple times.
-      Serial1.print(key);
+      lcd.setCursor(20,0);lcd.print("          ");
+      lcd.setCursor(18-Ct-Dt,0);
+      lcd.print(SEX0);
       //Serial1.print(Ct);Serial1.print(Dt);Serial1.print("key:");Serial1.print(key);Serial1.println(" Ex:" + SEX0);
       //yoink.PuttaCursor(FarRight-Ct-Dt) \\start FarRight, move left as many times as are strings in result
       //yoink.print(SEX1)
@@ -80,38 +88,41 @@ void DoMath(){              //okay here we go this is where i try to write code 
     SEX0 = String("");
     Ct=0;
     Dt=0;
-    Serial1.println("KILL");
+    lcd.setCursor(0,0);
+    String killmess = "KILLEMALLKILLEMALLKILLEMALLKILLEMALLKILLEMALLKILLEMALL";
+    for (byte i = 0; i < killmess.length(); i++) {
+      lcd.print(killmess[i]);
+      delay(10);}
+    delay(100);lcd.clear();
   }
   if (key=='*'||key=='/'||key=='-'||key=='+'||key=='='){    //if operator key pressed
     if (key=='*'){oper='*';}
     if (key=='/'){oper='/';}
     if (key=='-'){oper='-';}
     if (key=='+'){oper='+';}
-    Serial1.println(oper);
-        //yoink.PuttaCursor(OnDaLeft)
-        //yoink.print(oper);}
+    Ct=0;Dt=0;
+    lcd.setCursor(18,2);lcd.print(key);
     Calculate();
   }
 
 }
 
 void Calculate(){  // okay by here we should have SEX0 as a string containing whatever buttons we pushed and the desired operator
-if (SEX1==""){E1=EX;SEX1=SEX0;} //if first time, move the expressions around so we can get the second expression
-else      {E2=EX;SEX2=SEX0;}
+if (SEX1==""){E1=EX;SEX1=SEX0;SEX0=String("");} //if first time, move the expressions around so we can get the second expression
+else      {E2=EX;SEX2=SEX0;SEX0=String("");
   if (oper =='*') {E1=E1*E2;}
   if (oper =='/') {E1=E1/E2;}
   if (oper =='-') {E1=E1-E2;}
   if (oper =='+') {E1=E1+E2;}
-  
+  SEX3=String(E1);}
 if ((E1!=0)&&(key=='=')){
-  Serial1.println(E1);
-  Serial1.print(E1);Serial1.print(oper);Serial1.print(E2);
-}
-SEX0 = String("");
-SEX1 = String("");
-Ct=0;
-Dt=0;
-E1,E2,EX=0;
+  Ct=0;
+  Dt=0;
+  calc=true;}
+lcd.setCursor(0,0);lcd.print(SEX0);
+lcd.setCursor(0,1);lcd.print(SEX1);
+lcd.setCursor(0,2);lcd.print(SEX2);
+lcd.setCursor(0,3);lcd.print(SEX3);
 }
 
 //void DisplayResult(){
